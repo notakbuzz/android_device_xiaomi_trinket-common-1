d09cfb35055e809c00101c8cc87bc6ca4f7bf5fb
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    trinket-common: board: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9
    Signed-off-by: andrraa <andraramadanp@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,7 +158,7 @@ TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
