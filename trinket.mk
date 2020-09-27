48bd8294dd7e84c9ff76567ea4a69ea236f2478f
anayw2001 <awadhera@berkeley.edu>
2020-09-27 12:28:37 -0300

    trinket-common: power: Build new qcom power hal
    Also, drop some stuff used for our previous hal
    
    Signed-off-by: andrraa <andraramadanp@outlook.com>

--- trinket.mk
+++ trinket.mk
@@ -274,7 +274,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
